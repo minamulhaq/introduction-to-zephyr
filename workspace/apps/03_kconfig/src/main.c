fd756d3de58ff44ed6330ba98a90ce3ce749946d
muhammad.inamulhaq <muhammad.inamulhaq@ezono.com>
2026-02-06 13:00:38 +0100

    flashed first

--- workspace/apps/03_kconfig/src/main.c
+++ workspace/apps/03_kconfig/src/main.c
@@ -6,12 +6,13 @@ static const int32_t sleep_time_ms = 1000;
 
 int main(void){
     
+    static uint32_t count = 0;
     uint32_t rnd;
     
     for (;;) {
         rnd = sys_rand32_get();
         
-        printk("Random value: %u\r\n", rnd);
+        printk("Random value %ld: %u\r\n", count++, rnd);
         k_msleep(sleep_time_ms);
         
     }
