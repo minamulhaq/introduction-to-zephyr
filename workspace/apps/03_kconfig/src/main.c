397b95d77f8c1ed07beb39fa9d8cda34b0a20a4b
muhammad.inamulhaq <muhammad.inamulhaq@ezono.com>
2026-02-06 13:47:39 +0100

    03 challenge kconfig

--- workspace/apps/03_kconfig/src/main.c
+++ workspace/apps/03_kconfig/src/main.c
@@ -1,20 +1,24 @@
 #include <zephyr/random/random.h>
 
+#ifdef CONFIG_SAY_HELLO
+#include <say_hello.h>
+#endif
 
 static const int32_t sleep_time_ms = 1000;
 
-
-int main(void){
-    
+int main(void) {
     static uint32_t count = 0;
     uint32_t rnd;
-    
+
     for (;;) {
         rnd = sys_rand32_get();
-        
-        printk("Random value %ld: %u\r\n", count++, rnd);
+        double rnd_float = (double)rnd / (UINT32_MAX + 1.0);
+
+        printk("Random value %d: %.3f\r\n", count++, rnd_float);
+#ifdef CONFIG_SAY_HELLO
+        say_hello();
+#endif
         k_msleep(sleep_time_ms);
-        
     }
     return 0;
 }
